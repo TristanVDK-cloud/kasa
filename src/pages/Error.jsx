471e4c9010f27ec4f9fc9a9c66e00d00a3278353
TristanVDK <calgara@me.com>
2026-04-20 15:45:47 +0200

    Intégration de la page d'erreur 404 avec son style personnalisé ainsi que la redirection vers l'accueil.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,15 @@
 import { Link } from "react-router-dom";
-import './Error.scss'
+import '../styles/pages/_error.scss';
 
 
+function Error() {
+  return (
+    <div className="error-page">
+      <h1 className="error-page__title">404</h1>
+      <p className="error-page__message">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-page__link">Retourner sur la page d'accueil</Link>
+    </div>
+  );
+}
+
+export default Error;
